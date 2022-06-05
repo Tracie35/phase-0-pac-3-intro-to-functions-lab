3bcf989ddd8dd3e6f3d129dd4cd92e0a07f64823
Tracie35 <kariukitracie@gmail.com>
2022-06-05 18:37:59 +0300

    done with assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+function whisper(string) {
+    return string .toLowerCase();
+}
+console.log(string === String.toUpperCase())
+console.logwhisper (string) .toLowerCase();
+
+function 
+sayHiToHeadphonedRoommate(string) {
+    return string .toUpperCase();
+}
+function sayHiToHeadphonedRoommate(string){
+    return string .toLowerCase();
+
+}
